332d4f1be51b1f08b64bce6f36b47cdd8e1f336e
knitterJ <knitter.m.jacob@gmail.com>
2022-01-27 13:13:58 +0100

    coding challenge daily D7

---
+++ Elementary/count-digits.ts
@@ -0,0 +1,24 @@
+//You need to count the number of digits in a given string. 
+// Input: A Str.
+// Output: An Int. 
+
+
+function countDigits(text: string): number {
+    let count = 0;
+    for (let i=0; i < text.length; i++){
+            if(text[i].match(/\d/)){
+            count++}
+        } 
+    return count;
+}
+
+
+
+console.log(countDigits('hi'));
+console.log(countDigits('who is 1st here'));
+console.log(countDigits('my numbers is 2'));
+console.log(countDigits('This picture is an oil on canvas ' +
+ 'painting by Danish artist Anna ' +
+ 'Petersen between 1845 and 1910 year'));
+console.log(countDigits('5 plus 6 is'));
+console.log(countDigits(''));
