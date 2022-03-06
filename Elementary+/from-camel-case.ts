60c2f67fbf59474a67a1a416fa14da02c739e932
knitterJ <knitter.m.jacob@gmail.com>
2022-03-06 03:50:51 +0100

    coding challenge daily D40

---
+++ Elementary+/from-camel-case.ts
@@ -0,0 +1,14 @@
+// Your mission is to convert the name of a function (a string) from CamelCase ("MyFunctionName") into the Python format ("my_function_name") where all chars are in lowercase and all words are concatenated with an intervening underscore symbol "_".
+//
+// Input: A function name as a CamelCase string.
+//
+// Output: The same string, but in under_score.
+
+function fromCamelCase(name: string): string {
+    var underscoreversion = name.split(/(?=[A-Z])/).join(" ").toLowerCase().replace(/ /g, "_");
+    // console.log(underscoreversion);
+    return underscoreversion;
+}
+
+console.log(fromCamelCase('MyFunctionName')); // my_function_name
+console.log(fromCamelCase('IPhone')); // i_phone
