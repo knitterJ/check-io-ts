0c5927836d72141313e0e868ea59205bb468e276
knitterJ <38995057+knitterJ@users.noreply.github.com>
2022-06-05 16:07:17 +0200

    Delete Other directory

--- Other/Testparser/readme.txt
+++
@@ -1,1 +0,0 @@
-My professor published a final test in txt file, and he's forced to review each of the student's test manually. In order to save on effort, I decided to write the script for him. 
--- Other/Testparser/testparser.js
+++
@@ -1,6 +0,0 @@
-// Node implements File I/O using simple wrappers around standard POSIX functions. The Node File System (fs) module can be imported using the following syntax −
-var fs = require('fs');
-
-
-var text = fs.readFileSync("./mytext.txt").toString('utf-8');
-console.log(text);
