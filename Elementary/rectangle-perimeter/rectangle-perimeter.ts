ee1aa9917aff103b1f53480644e7b1308542c219
J <knitter.m.jacob@gmail.com>
2025-04-04 15:07:49 +0200

    f

--- Elementary/rectangle-perimeter/rectangle-perimeter.ts
+++ Elementary/rectangle-perimeter/rectangle-perimeter.ts
@@ -1,6 +1,6 @@
  // This function should take two positive numbers
  // (length and width) as inputs and return the perimeter of a rectangle.
-import myImage from '/media/jake/Samsung_T5/4PROGRAMMINGBIBLE/DuzeProjekty/CodeChallenges/check-io-ts/Elementary/perimeter.png'
+import myImage from './perimeter.png'
 const imageElement = <img src={myImage} alt="My Image" />;
 function rectanglePerimeter(length: number, width: number): number {
     return length*2+width*2;
