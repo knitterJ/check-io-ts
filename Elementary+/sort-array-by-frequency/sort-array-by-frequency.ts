27bdc62f84a411e090d78de86aa15f7404e1d1a8
J <knitter.m.jacob@gmail.com>
2025-04-11 00:33:07 +0200

    7TH DAY

--- Elementary+/sort-array-by-frequency/sort-array-by-frequency.ts
+++ Elementary+/sort-array-by-frequency/sort-array-by-frequency.ts
@@ -1,14 +1,23 @@
-
+ // Sort the given Array so that its elements should be grouped and those groups end up in the decreasing frequency order, that is, the number of times element appears in Array.
+ // If two elements have the same frequency, their groups should end up in the same order as the first appearance of element in the Array.
 
 function frequencySort<T extends number | string>(items: T[]): T[] {
 
+    // create a following map {1: 2, 2:1, 3: 2} { item : amountOfTimes }
+    // "one appeared two times, two appeared one time, three appeared two times"
+    // Let's define it first
+    const FirstFrequecyMap: Map<T, number> = new Map;
+
+    for(const item of items){
+
+      console.log(FirstFrequecyMap.set(item, FirstFrequecyMap.get(item) + 1 || 0));
+    };
 
     return [];
 }
 
-
-console.log(nonUniqueElementz([1, 2, 3, 1, 3]));
-console.log(nonUniqueElementz([1, 2, 3, 4, 5]));
-console.log(nonUniqueElementz([5, 5, 5, 5, 5]));
-console.log(nonUniqueElementz([10, 9, 10, 10, 9, 8]));
-console.log(nonUniqueElementz(["bob", "bob", "blaa"]));
+console.log(frequencySort([1, 2, 3, 1, 3]));
+console.log(frequencySort([1, 2, 3, 4, 5]));
+console.log(frequencySort([5, 5, 5, 5, 5]));
+console.log(frequencySort([10, 9, 10, 10, 9, 8]));
+console.log(frequencySort(["bob", "bob", "blaa"]));
