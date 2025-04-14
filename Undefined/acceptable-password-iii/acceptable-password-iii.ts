43a274aa96d1102386d09f7d533f6375c91d0934
J <knitter.m.jacob@gmail.com>
2025-04-14 23:57:19 +0200

    coding challenge daily - coming back after two days pause

---
+++ Undefined/acceptable-password-iii/acceptable-password-iii.ts
@@ -0,0 +1,6 @@
+// In this mission you need to create a password verification function.
+//
+// The verification conditions are:
+//
+//    the length should be bigger than 6;
+//    should contain at least one digit, but cannot consist of just digits.
--- Undefined/acceptable-passwrd-ii/acceptable-passwrd-ii.ts
+++ Undefined/acceptable-passwrd-ii/acceptable-passwrd-ii.ts
@@ -10,10 +10,18 @@ function isAcceptablePassword(password: string): boolean {
         // return conditionsAll.test(password);
         const hasCorrectLength = password.length > 6;
         const containsDigit = ['0','1','2','3','4','5','6','7','8','9'].some(digit => password.includes(digit));
-        return hasCorrectLength && containsDigit;
+        const soloDigits = /^\d+$/.test(password);
+        // /^\d+$/
+        // /: Delimiters indicating the start and end of the regular expression.
+        // ^: Anchor that matches the beginning of the string.
+        // \d+: Matches one or more digit characters (0-9). The + quantifier means "one or more occurrences".
+        // $: Anchor that matches the end of the string.
+
+        return hasCorrectLength && containsDigit && !soloDigits;
 }
 
 
 console.log(isAcceptablePassword("short"));
 console.log(isAcceptablePassword("muchlonger"));
 console.log(isAcceptablePassword("abcdefg8"));
+console.log(isAcceptablePassword("123452428"));
